e21747e1c93fa6eb1e5c1a276ff0b04827180ab0
mesmit22 <mesmit22@gmail.com>
2015-09-01 21:01:53 -0400

    Testing Git to see how it works

--- app/src/main/java/com/learning/myapplication/MainActivity.java
+++ app/src/main/java/com/learning/myapplication/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+        //testing GitHub to see how it works
     }
 
     @Override
